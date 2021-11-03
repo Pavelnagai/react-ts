56c464f12e3ba85d163ce4219c83f368eecd0d17
Pavelnagai <pavel.nagai@mail.ru>
2021-11-04 01:57:35 +0300

    NavLink

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import classes from "./Header.module.css";
+import s from "./Header.module.css";
 
 
 const Header = () => {
-    return <header className={classes.header}>
+    return <header className={s.header}>
         <img
             src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOyCtPSy5vZ6NNKe6nmW5aEAWDfv2R3bV72g&usqp=CAU'}/>
     </header>

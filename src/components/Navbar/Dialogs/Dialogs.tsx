6e39ea14c054c54052fa6570e5e3e4819c19e25a
Pavelnagai <pavel.nagai@mail.ru>
2021-11-04 16:21:47 +0300

    Dialogs NavLink

--- src/components/Navbar/Dialogs/Dialogs.tsx
+++ src/components/Navbar/Dialogs/Dialogs.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 import s from './Dialogs.module.css'
 
 const Dialogs = () => {
@@ -6,22 +7,21 @@ const Dialogs = () => {
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
                 <div className={s.dialog + " " + s.active}>
-                    Pavel
+                    <NavLink to='/dialogs/1'> Pavel </NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Andrei
+                    <NavLink to='/dialogs/2'> Andrei </NavLink>
                 </div>
                 <div className={s.dialog}>
-                    David
+                    <NavLink to='/dialogs/3'> David </NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4'> Anna </NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Anna
+                    <NavLink to='/dialogs/5'> Alina </NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Alina
-                </div>
-                <div className={s.dialog}>
-                    Sergei
+                    <NavLink to='/dialogs/6'> Sergei </NavLink>
                 </div>
             </div>
             <div className={s.messages}>

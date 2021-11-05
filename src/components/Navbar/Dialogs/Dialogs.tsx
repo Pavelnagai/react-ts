b1cc8f34a10500662908a7292b1210aefebe9ddc
Pavelnagai <pavel.nagai@mail.ru>
2021-11-05 22:04:54 +0300

    Dialogs props

--- src/components/Navbar/Dialogs/Dialogs.tsx
+++ src/components/Navbar/Dialogs/Dialogs.tsx
@@ -2,32 +2,46 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import s from './Dialogs.module.css'
 
+type DialogItemProps = {
+    id: number
+    name: string
+}
+const DialogItem = (props: DialogItemProps) => {
+    return (
+        <div className={s.dialog + " " + s.active}>
+            <NavLink to={'/dialogs/' + props.id}> {props.name} </NavLink>
+        </div>
+    )
+}
+
+type MessagePropsType = {
+    message: string
+}
+const Message = (props: MessagePropsType) => {
+    return (
+        <div>
+            <div className={s.message}>{props.message}</div>
+        </div>
+    )
+}
+
 const Dialogs = () => {
     return (
-        <div className={s.dialogs}>
-            <div className={s.dialogsItem}>
-                <div className={s.dialog + " " + s.active}>
-                    <NavLink to='/dialogs/1'> Pavel </NavLink>
+        <div>
+            <div className={s.dialogs}>
+                <div className={s.dialogsItem}>
+                    <DialogItem name='Pavel' id={1}/>
+                    <DialogItem name='Andrei' id={2}/>
+                    <DialogItem name='Alina' id={3}/>
+                    <DialogItem name='Anna' id={4}/>
+                    <DialogItem name='Make' id={5}/>
+                    <DialogItem name='David' id={6}/>
                 </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/2'> Andrei </NavLink>
+                <div className={s.messages}>
+                    <Message message="Hy"/>
+                    <Message message="Hello dad"/>
+                    <Message message="Year good"/>
                 </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/3'> David </NavLink></div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/4'> Anna </NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/5'> Alina </NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/6'> Sergei </NavLink>
-                </div>
-            </div>
-            <div className={s.messages}>
-                <div className={s.message}>Hy</div>
-                <div className={s.message}>Hello dad</div>
-                <div className={s.message}>Year good</div>
             </div>
         </div>
     )

de5a2dcaa904444ab6ca5174261678f1071cb199
Pavelnagai <pavel.nagai@mail.ru>
2021-11-10 17:46:15 +0300

    upgrade components

--- src/components/Navbar/Dialogs/Dialogs.tsx
+++ src/components/Navbar/Dialogs/Dialogs.tsx
@@ -3,23 +3,41 @@ import s from './Dialogs.module.css'
 import DialogItem from "./DialogsItem/DialogsItem";
 import Message from "./Message/Message";
 
-
 const Dialogs = () => {
+    let dialogsData = [
+        {id: 1, name:"Pavel"},
+        {id: 2, name: "Andrei"},
+        {id: 3, name: "Alina"},
+        {id: 4, name: "Anna"},
+        {id: 5, name: "Make"},
+        {id: 6, name: "David"}
+    ]
+    let messageData = [
+        {id: 1, message: "Hy"},
+        {id: 2, message: "Hello dad"},
+        {id: 3, message: "Your good friends"},
+        {id: 4, message: "Anna good girl"},
+        {id: 5, message: "Meat year"},
+        {id: 6, message: "David boy"}
+    ]
     return (
         <div>
             <div className={s.dialogs}>
                 <div className={s.dialogsItem}>
-                    <DialogItem name='Pavel' id={1}/>
-                    <DialogItem name='Andrei' id={2}/>
-                    <DialogItem name='Alina' id={3}/>
-                    <DialogItem name='Anna' id={4}/>
-                    <DialogItem name='Make' id={5}/>
-                    <DialogItem name='David' id={6}/>
+                    <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                    <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                    <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                    <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                    <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                    <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
                 </div>
                 <div className={s.messages}>
-                    <Message message="Hy"/>
-                    <Message message="Hello dad"/>
-                    <Message message="Year good"/>
+                    <Message message={messageData[0].message}/>
+                    <Message message={messageData[1].message}/>
+                    <Message message={messageData[2].message}/>
+                    <Message message={messageData[3].message}/>
+                    <Message message={messageData[4].message}/>
+                    <Message message={messageData[5].message}/>
                 </div>
             </div>
         </div>
--- src/components/Navbar/Profile/MyPosts/MyPosts.tsx
+++ src/components/Navbar/Profile/MyPosts/MyPosts.tsx
@@ -3,21 +3,27 @@ import classn from "./MyPosts.module.css"
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+    let postDate = [
+        {id:1, message: "Post 1", likeCheck: 12},
+        {id:2, message: "Post 2", likeCheck: 18},
+        {id:3, message: "Post 3", likeCheck: 11},
+        {id:4, message: "Post 4", likeCheck: 19},
+    ]
     return (
         <div className={classn.item}>
             <h3>My Posts</h3>
-            <Post message={"Post1"}
+            <Post message={postDate[0].message}
                   img={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgbvFfv0asMpnbwK_kbqMGgrTOeC0FR-lrXw&usqp=CAU"}
-                  likeCheck={35}/>
-            <Post message={"Post2"}
+                  likeCheck={postDate[0].likeCheck}/>
+            <Post message={postDate[1].message}
                   img={"https://images.beinsports.com/_5WS_TDaEkSE8qs8kMH016kcRg0=/670x424/smart/3992496-000_9PR8WL.jpg"}
-                  likeCheck={59}/>
-            <Post message={"Post3"}
+                  likeCheck={postDate[1].likeCheck}/>
+            <Post message={postDate[2].message}
                   img={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxSYpyjZbhuMDJpeZly0eh4vt5XDE4OvCotWSrmlzgjilQgf49eXkrgkGo5vTsUdfdHxs&usqp=CAU"}
-                  likeCheck={2}/>
-            <Post message={"Post4"}
+                  likeCheck={postDate[2].likeCheck}/>
+            <Post message={postDate[3].message}
                   img={"https://as01.epimg.net/en/imagenes/2021/09/26/soccer/1632648990_268324_noticia_normal_recorte1.jpg"}
-                  likeCheck={104}/>
+                  likeCheck={postDate[3].likeCheck}/>
         </div>
     )
 }

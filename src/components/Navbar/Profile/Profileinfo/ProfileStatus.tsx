500fb2e0ad2314e5c8bdce5816adbccc93860d84
Pavelnagai <pavel.nagai@mail.ru>
2022-02-03 17:11:21 +0300

    status

---
+++ src/components/Navbar/Profile/Profileinfo/ProfileStatus.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export class ProfileStatus extends React.Component<any, any> {
+    state = {
+        editMode: false
+    }
+
+    activateEditeMode() {
+        this.setState({
+            editMode: true
+        })
+    }
+
+    deactivateEditeMode() {
+        this.setState({
+            editMode: false
+        })
+    }
+
+    render() {
+        return (
+            <div>{
+                !this.state.editMode &&
+                <div>
+                    <span onDoubleClick={() => this.activateEditeMode()}>{this.props.status}</span>
+                </div>
+            }
+                {
+                    this.state.editMode &&
+                    <div>
+                        <input autoFocus={true} onBlur={() => this.deactivateEditeMode()} value={this.props.status}
+                               type="text"/>
+                    </div>
+                }
+
+
+            </div>
+
+        )
+
+    }
+}
